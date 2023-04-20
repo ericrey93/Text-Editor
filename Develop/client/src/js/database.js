48dfbdca5ead264136ddff5b41596e9a6fbe000b
Eric Reyes <ericreyes1109@gmail.com>
2023-04-20 17:27:51 -0500

    finished all the todos and updated the README

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Update the database.');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({value: content, id: 1});
+  const result = await request;
+  console.log(result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('showing the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result?.value;
+};
+
 
 initdb();
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Text-Editor
+
+## Description
+Here is yet again, another note taker! Inside the app, you will see a button that lets you install this app to use whenever you like for any notes you need!
+
+## Tech Used
+ - babel
+ - webpack
+ - express
+ - node.js
+refer to the package.json inside the client and server folders for the full list of dependencies used for this app.
+
+## Deployment 
+
+[Here]() is the link to the deployed app on Heroku.
+
+## License
+Please refer to the repo for the license
+
+## Contribution
+
+[Here]() is the repo in which the app was created by me!
